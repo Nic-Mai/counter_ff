904c53470e9e594087a41e0d5e6e9fcf710d22e3
Nicolas Maier <Nicolas.Maier@edu.hefr.ch>
2020-08-18 17:29:56 +0200

    alarm sound

--- main.py
+++ main.py
@@ -6,30 +6,32 @@ import mss
 import sys
 import time
 import Levenshtein
+import playsound
 
 text_enemy_surrend = 'Enemy team agreed to a surrend with x votes for and x against'
 text_ally_surrend_after = 'Your team agreed to a surrender with x votes for and x against'
 text_ally_surrend = 'has started a surrender vote. Type /surrender or /nosurrender'
 enemy_surrend_max_distance = 10
 ally_surrend_max_distance = 8
+sound_repetitions = 8
+
 
 def find_closest_match(string, text):
     closest_match = -1
     closest_distance = -1
     for i in range(len(string) + len(text)):
-        distance = Levenshtein.distance(string[i-len(text) if i-len(text) >= 0 else 0 : i], text)
+        distance = Levenshtein.distance(string[i-len(text) if i-len(text) >= 0 else 0: i], text)
         if distance < closest_distance or closest_distance < 0:
             closest_match = i
             closest_distance = distance
 
-    print("- Closest match:")
-    print(string[closest_match-len(text) if closest_match-len(text) >= 0 else 0 : closest_match])
     return closest_distance
 
-kernel_erode_v = numpy.ones((2,1), numpy.uint8)
-kernel_erode_h = numpy.ones((1,2), numpy.uint8)
-kernel_dilate = numpy.ones((5,5), numpy.uint8)
-kernel_final = numpy.ones((2,1), numpy.uint8)
+
+kernel_erode_v = numpy.ones((2, 1), numpy.uint8)
+kernel_erode_h = numpy.ones((1, 2), numpy.uint8)
+kernel_dilate = numpy.ones((5, 5), numpy.uint8)
+kernel_final = numpy.ones((2, 1), numpy.uint8)
 
 sct = mss.mss()
 monitor = {'left': 10, 'top': 1062, 'width': 680, 'height': 190}
@@ -68,6 +70,15 @@ while True:
 
     print("=== Detected:")
     print(detected_string)
+    print("===")
+
+    detected_string2 = '''Type /help for a list of commands
+Enemy team has destroyed the first turret! (Bonus Gold: )
+DrG Minimayer has started a surrender vote. Type /surrender or
+/nosurrender,
+Your team agreed to a surrender with | votes for and 0 against.
+
+'''
 
     detected_string = detected_string.replace('\n', ' ').replace('\r', '')
 
@@ -75,16 +86,21 @@ while True:
     ally_surrend_distance = find_closest_match(detected_string, text_ally_surrend)
     ally_surrend_after_distance = find_closest_match(detected_string, text_ally_surrend_after)
 
-    print("--------", enemy_surrend_distance, ally_surrend_distance, ally_surrend_after_distance)
+    print('enemy_surrend_distance:', enemy_surrend_distance, 'ally_surrend_distance:', ally_surrend_distance, 'ally_surrend_after_distance:', ally_surrend_after_distance)
 
     if enemy_surrend_distance < enemy_surrend_max_distance and enemy_surrend_distance < ally_surrend_after_distance:
-        print("ENEMY SURREND")
-        time.sleep(2)
-    
-    if ally_surrend_distance < ally_surrend_max_distance:
-        print("ALLY SURREND")
-        time.sleep(2)
-    
+        while True:
+            for i in range(sound_repetitions):
+                print('ENEMY SURREND')
+                playsound.playsound('annoying_alarm_clock_sound.wav')
+            exit()
+    elif ally_surrend_distance < ally_surrend_max_distance:
+        while True:
+            for i in range(sound_repetitions):
+                print('ALLY SURREND')
+                playsound.playsound('annoying_alarm_clock_sound.wav')
+            exit()
+
     frame_count += 1
     if frame_count == fps:
         t1 = time.time()
